ea41dfc557d5fc3f505044d4d904b9b003fe1824
Kristina Laude <kristinalaude@gmail.com>
2016-05-30 17:26:54 -0700

    Adding keydown and keyup functions

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         500,
         function() {
         $(this).hide();
-        $(this).css("left", "-212px");
+        $(this).css("left", "520px");
       }
     );
 
